fba0c4e13ba6bbe7bfa4f057e44d8c45ddcb13f9
QuangVu <truongquangvu41@gmail.com>
2025-07-01 01:24:54 +0700

    fix cc form style

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -8,7 +8,7 @@ function UpdateProfileForm({ children, guest }) {
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
-    <form action={updateGuest} className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
+    <form action={updateGuest} className='bg-primary-900 !py-8 !px-12 text-lg flex gap-6 flex-col'>
       <div className='space-y-2'>
         <label>Full name</label>
         <input
